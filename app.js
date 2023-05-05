a999c6af4e13b37450caece2ac9fec29dc7fed10
Rafael Garcia <91357930+RafaelGarcia500@users.noreply.github.com>
2023-05-05 00:13:18 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const btns = document.querySelectorAll(`button`)
+    
+   let playerScore = 0
+   let computerScore = 0
+   let gameRound = 0
+
+
 
     btns.forEach((btn) => { /* inputs the choice of the player and the computer */
     btn.addEventListener(`click`,() =>{
@@ -8,13 +14,49 @@ const btns = document.querySelectorAll(`button`)
         clearButtons()
         computerChoice()
         showResults(btn.id)
-
         btn.appendChild(para)
+        gameRound++
+        if(gameRound >= 5){
+            showWinner()
+            
+        } 
         
     })
 })
-
  
+ function showWinner(){
+    if(playerScore > computerScore ){
+        let roundOutcome = document.createElement(`h3`)
+        let restartInput = document.createElement(`button`)
+        restartInput.classList.add(`restartButton`)
+        roundOutcome.classList.add(`resultText`)
+        roundOutcome.textContent = `the player has won the match!`
+        restartInput.textContent = `Play again?`
+        restartInput.addEventListener(`click`,refreshPage)
+        clearRButtons()
+        clearBbutton()
+        document.querySelector(`body`).appendChild(restartInput)
+        document.querySelector(`body`).appendChild(roundOutcome)
+        
+
+    }
+    else if( playerScore < computerScore){
+        let roundOutcome = document.createElement(`h3`)
+        let restartInput = document.createElement(`button`)
+        restartInput.classList.add(`restartButton`)
+        roundOutcome.classList.add(`resultText`)
+        roundOutcome.textContent = `the computer has won the match!`
+        restartInput.textContent = `Play again?`
+        restartInput.addEventListener(`click`,refreshPage)
+        clearRButtons()
+        clearBbutton()
+        document.querySelector(`body`).appendChild(restartInput)
+        document.querySelector(`body`).appendChild(roundOutcome)
+        
+        
+
+    }
+ }
 
 
  function computerChoice() {             /*Selects a random id from the buttons          */
@@ -24,7 +66,7 @@ const btns = document.querySelectorAll(`button`)
 
     const cpara = document.createElement(`p`);
     cpara.classList.add(`computerChoice`);
-    cpara.textContent = `Computer has chosen ${computerSelection}`;
+    cpara.textContent = `Computer has chosen ${computerSelection}`
     clearcButtons();
     choices[randomIndex].appendChild(cpara);
 }
@@ -43,6 +85,7 @@ function showResults(playerDecition){  /* checks the result */
     else if(playerDecition === `Rock` && computerDecition === `Scissors`||
             playerDecition === `Paper` && computerDecition === `Rock`||
             playerDecition === `Scissors` && computerDecition === `Paper`){
+                playerScore++
                 let resultText = document.createElement(`h2`)
                 resultText.classList.add(`resultText`)
                 resultText.textContent = `Awesome, ¡you just won!`
@@ -50,12 +93,14 @@ function showResults(playerDecition){  /* checks the result */
                 document.querySelector(`body`).appendChild(resultText)
             }
     else{
+        computerScore++
         let resultText = document.createElement(`h2`)
             resultText.classList.add(`resultText`)
             resultText.textContent = `That is too bad, you lose..`
             clearRButtons()
             document.querySelector(`body`).appendChild(resultText)
     }
+    
   }
 
 
@@ -75,5 +120,21 @@ function showResults(playerDecition){  /* checks the result */
     if (existingPara){
         existingPara.parentNode.removeChild(existingPara)
     }
+ }
+ function clearBbutton(){
+    const existingPara = document.querySelector(`.restartButton`)
+    if(existingPara){
+        existingPara.parentNode.removeChild(existingPara)
+ }
 
  }
+
+ function refreshPage() {
+     playerScore = 0
+     computerScore = 0
+     gameRound = 0
+    clearButtons()
+    clearcButtons
+    clearRButtons()
+    location.reload();
+  }
